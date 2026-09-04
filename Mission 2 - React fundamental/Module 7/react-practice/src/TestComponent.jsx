3672e8629735b700279b38f871e1a96aafd68526
Abir Das <abirdas422@gmail.com>
2026-09-04 22:02:14 +0600

    Module 7 1st component make done

---
+++ Mission 2 - React fundamental/Module 7/react-practice/src/TestComponent.jsx
@@ -0,0 +1,16 @@
+const TestComponent = () => {
+  return (
+    <>
+        <div>
+        <h1>Hello, React!</h1>
+        <p>This is a test component.</p>
+        </div>
+        <div>
+            <span>React is a JavaScript library for building user interfaces.</span>
+            <span> It allows developers to create reusable UI components and manage the state of their applications efficiently.</span>
+        </div>
+    </>
+  );
+};
+
+export default TestComponent;
